46a186e160512ccc555b6f0973de6cd44dccc02b
Andrea Laserna <andrealaserna1@gmail.com>
2024-08-07 09:11:20 +0800

    javascript hiding items for mobile view

--- Andrea Portfolio/myscripts.js
+++ Andrea Portfolio/myscripts.js
@@ -10,3 +10,18 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
+
+function removeTwoSlides(){
+    console.log('Script is running');
+    const items = document.querySelectorAll('.item');
+    if (window.innerWidth <= 1075) {
+        for (let i = items.length - 2; i < items.length; i++) {
+            items[i].classList.add('hidden');
+        }
+    } else {
+        items.forEach(item => item.classList.remove('hidden'));
+        }
+}
+
+window.addEventListener('resize', removeTwoSlides);
+window.addEventListener('load', removeTwoSlides);
